2389e021de7b533a144fc995a6853a0b2228eaca
parth022002 <parthahuja021102@gmail.com>
2024-05-29 19:25:41 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['nhmlkfspbccvjzqgmyye.supa.co'],
+    }
+};
 
 export default nextConfig;
