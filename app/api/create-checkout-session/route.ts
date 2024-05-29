6f5bd5ddb52170051bbb1e87e007bfc47e71a110
parth022002 <parthahuja021102@gmail.com>
2024-05-29 20:05:27 +0530

    update

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,7 +1,6 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from "next/headers";
 import { NextResponse } from 'next/server';
-
 import { stripe } from '@/libs/stripe';
 import { getURL } from '@/libs/helpers';
 import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
@@ -12,9 +11,8 @@ export async function POST(
   const { price, quantity = 1, metadata = {} } = await request.json();
 
   try {
-    const supabase = createRouteHandlerClient({ 
-      cookies
-      });      const {
+    const supabase = createRouteHandlerClient({ cookies });
+    const {
       data: { user }
     } = await supabase.auth.getUser();
 
@@ -26,7 +24,7 @@ export async function POST(
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       billing_address_collection: 'required',
-      customer,
+      customer: customer, // Use the customer directly if it's already a string
       line_items: [
         {
           price: price.id,
@@ -36,7 +34,6 @@ export async function POST(
       mode: 'subscription',
       allow_promotion_codes: true,
       subscription_data: {
-        trial_from_plan: true,
         metadata
       },
       success_url: `${getURL()}/account`,
--- libs/stripe.ts
+++ libs/stripe.ts
@@ -3,8 +3,8 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ?? '',
   {
-    apiVersion: '2022-11-15',
-    appInfo: {
+    apiVersion: '2024-04-10',
+    appInfo:{
       name: 'Spotify Clone',
       version: '0.1.0'
     }
