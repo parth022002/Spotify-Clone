3ae988d6824cd64fdf8379a0ade5e9bf7a184519
parth022002 <parthahuja021102@gmail.com>
2026-07-16 08:03:32 +0530

    fix: add unsplash to next image configuration domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
